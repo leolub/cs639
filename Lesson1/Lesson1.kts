de2d662f87ab80a4d6a350aec53318ae89586417
leolub <3525794821@qq.com>
2025-10-11 11:13:15 -0400

    reorganize file

---
+++ Lesson1/Lesson1.kts
@@ -0,0 +1,37 @@
+//Hello Kotlin
+fun printHello() {
+    println ("Hello World")
+}
+printHello()
+
+//Learn about operators and types
+//Step 1: Explore numeric operators
+1+1
+//kotlin.Int = 2
+53-3
+//kotlin.Int = 50
+50/10
+//kotlin.Int = 5
+1.0/2.0
+//kotlin.Double = 0.5
+2.0*3.5
+//kotlin.Double = 7.0
+6*50
+//kotlin.Int = 300
+6.0*50.0
+//kotlin.Double = 300.0
+6.0*50
+//kotlin.Double = 300.0
+2.times(3)
+//kotlin.Int = 6
+3.5.plus(4)
+//kotlin.Double = 7.5
+2.4.div(2)
+//kotlin.Double = 1.2
+
+
+//Step 2: Practice using types
+val i: Int = 6
+val b1 = i.toByte()
+println(b1)
+// result:6
